41bcfbde44dc3aaeec5abf7026840e3b319c31ad
Jakub Korzonek <jkorzonek2@krakow.newtechteam.pl>
2023-05-25 09:35:59 +0200

    Dodaj pliki projektów.

---
+++ Vector/Vector.cpp
@@ -0,0 +1,50 @@
+﻿#include <vector>
+#include <iostream>
+#include <stdexcept>
+
+using namespace std;
+
+vector<int> zakodowaneHaslo(const string& haslo) {
+    vector<int> zakodowaneHaslo;
+
+    for (char c : haslo) {
+        if (c >= 'a' && c <= 'z') {
+            zakodowaneHaslo.push_back(c - 'a' + 1);
+        }
+        else if (c >= 'A' && c <= 'Z') {
+            zakodowaneHaslo.push_back(c - 'A' + 27);
+        }
+        else if (c >= 'O' && c <= 'g') {
+            zakodowaneHaslo.push_back(c - '0' + 53);
+        }
+        else {
+            throw std::invalid_argument("zly znak w haslo");
+        }
+    }
+
+    return zakodowaneHaslo;
+}
+
+int main()
+{
+    string haslo = "Admin123";
+
+
+
+    try {
+        std::vector<int> encoded = zakodowaneHaslo(haslo);
+
+        cout << "zakodowane haslo:";
+        for (int digit : encoded) {
+            cout << digit << " ";
+        }
+
+    }
+
+    catch (const invalid_argument& e) {
+        cout << "wyjatek: " << e.what() << endl;
+    }
+    return 0;
+
+
+}
